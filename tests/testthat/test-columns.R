a0f8681868a668b2dc3a5abc5bb71e0b29400b33
Daniel Falbel <dfalbel@gmail.com>
2019-08-14 14:27:39 -0300

    use smaller model to test columns

--- tests/testthat/test-columns.R
+++ tests/testthat/test-columns.R
@@ -5,7 +5,7 @@ source("utils.R")
 test_succeeds("hub_text_embedding_column can load a module", {
 
   column <- hub_text_embedding_column("sentence",
-                                      "https://tfhub.dev/google/universal-sentence-encoder/1")
+                                      "https://tfhub.dev/google/nnlm-en-dim128/1")
   expect_true(reticulate::py_has_attr(column, "name"))
 })
 
